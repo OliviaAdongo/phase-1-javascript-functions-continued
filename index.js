096eca97e31a81eb7746b272960d9a1109821512
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-07 22:10:09 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+
+function satudayFun(toDO="roller-skate"){
+    return `This Saturday, I want to ${toDO}!`;
+}
+satudayFun();
+satudayFun("bathe my dog");
+
+function mondayWork(day ="go to the office"){
+    return `This Monday, I will ${day}.`;
+
+}
+mondayWork();
+mondayWork("Work from home.");
+
+function wrapAdjective(testing = "*"){
+    return function(mypar = "special"){
+        return `You are ${testing}${mypar}${testing}!`;
+    }
+}
+wrapAdjective()("a hardworker");
+wrapAdjective("||",)("a dedicated programmer");
