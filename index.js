f3d2520fa6e07f134a8c0e7e35e66493bf44277d
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-08 08:04:08 +0300

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ function mondayWork(day ="go to the office"){
     return `This Monday, I will ${day}.`;
 
 }
-mondayWork();
-mondayWork("Work from home.");
+
 
 function wrapAdjective(sample = "*"){
     return function(mypar = "special"){
         return `You are ${sample}${mypar}${sample}!`;
     }
 }
-wrapAdjective()("a hardworker");
-wrapAdjective("||",)("a dedicated programmer");
